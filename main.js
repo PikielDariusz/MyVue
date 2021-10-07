21d9aa8b37fb0f58ad2bc22b06f1ad079c64c8b4
PikielDariusz <49079311+PikielDariusz@users.noreply.github.com>
2021-10-07 21:17:11 +0200

    Update main.js
    lekcja 8

--- main.js
+++ main.js
@@ -2,35 +2,58 @@ const app = Vue.createApp({
     data() {
         return {
           cart: 0,
-            message: 'Hello Vue!',
+            product: 'Socks',
+            brand: 'Vue Mastery',
             description: 'reaktywny?',
-            image: './assets/images/socks_blue.jpg',
+            selectedVariant: 0,
             url: 'http://uniqa.pl',
-            inventory: 3,
-            varProductAvailable: (this.inventory > 0 && this.cart<this.inventory),
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-              ]
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 3 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' , quantity: 0},
+                { id: 2236, color: 'red', image: './assets/images/socks_blue_red_circle.jpg' , quantity: 15}
+              ],
+            onSale: true
         }
     },
-    created: function() {
-        this.varProductAvailable = (this.inventory > 0 && this.cart<this.inventory);
-      },
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        productAvailable() {
+            return (this.inventory > 0 && this.cart < this.inventory);
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inventory(){
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            if(this.onSale){
+                return this.title + ' is on sale'
+            }
+        }
+    },
     methods: {
         addToCart(){
             this.cart += 1;
-            this.varProductAvailable = (this.inventory > 0 && this.cart<this.inventory);
+            //this.varProductAvailable = (this.inventory > 0 && this.cart<this.inventory);
         },
         removeCart(){
             if(this.cart > 0){
                 this.cart -= 1;
-                this.varProductAvailable = (this.inventory > 0 && this.cart<this.inventory);
+                //this.varProductAvailable = (this.inventory > 0 && this.cart<this.inventory);
             }            
         },
-        updateImage(variantImage){
-            this.image = variantImage;
+        updateVariant(variantIndex){
+            if(this.cart == 0 ) {
+                this.selectedVariant = variantIndex;
+                console.log(variantIndex);
+            } else {
+                console.log('Zmiana typu niemożliwa')
+            }
         }
     },
 })
